5e5d22887eb23c0c26ac2bb10aee20ab3651e927
Garv2468 <106488245+Garv2468@users.noreply.github.com>
2026-06-10 23:09:51 +0530

    Add files via upload

--- pose_detector.py
+++ pose_detector.py
@@ -3,52 +3,36 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import video_loader as vl
-import time
 
-l = []
-modelPath = r"pose_landmarker_heavy.task"
+MODEL_PATH = "/home/garv/Python/Open_cv/pose_landmarker_heavy.task"
 
-baseOptions = python.BaseOptions(model_asset_path=modelPath)
-options = vision.PoseLandmarkerOptions(base_options=baseOptions
-                                       , output_segmentation_masks=False
-                                       , running_mode=vision.RunningMode.IMAGE
-                                       , num_poses=1
-                                       , min_pose_detection_confidence=0.5
-                                       , min_tracking_confidence=0.5)
-poseEstimator = vision.PoseLandmarker.create_from_options(options)
+def init_detector():
+    base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
+    options = vision.PoseLandmarkerOptions(
+        base_options=base_options,
+        output_segmentation_masks=False,
+        running_mode=vision.RunningMode.VIDEO,
+        num_poses=1,
+        min_pose_detection_confidence=0.5,
+        min_tracking_confidence=0.5
+    )
+    return vision.PoseLandmarker.create_from_options(options)
 
+def detect_video(cap, detector, fps):
+    keypoints_sequence = []
+    frame_number = 0
 
-cap, video_data = vl.load_video("test2.mp4")
-print(f"Processing video at {video_data['fps']} FPS.")
+    for frame in vl.frame_generator(cap):
+        timestamp_ms = int(frame_number * 1000 / fps)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+        result = detector.detect_for_video(mp_image, timestamp_ms)
 
+        if not result.pose_world_landmarks:
+            keypoints_sequence.append(None)
+        else:
+            keypoints_sequence.append(result.pose_world_landmarks[0])
 
-for frame in vl.frame_generator(cap):
+        frame_number += 1
 
-
-    rgbFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    mpImg = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgbFrame)
-
-    result = poseEstimator.detect(mpImg)
-
-    if result.pose_world_landmarks:
-        world_landmarks = result.pose_world_landmarks[0]
-        rightWrist = world_landmarks[16]
-        l.append((rightWrist.x, rightWrist.y, rightWrist.z))
-
-
-
-
-    cv2.imshow("Video Playback", frame)
-
-
-
-
-
-
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-vl.release_video(cap)
-cv2.destroyAllWindows()
-print(l)
+    return keypoints_sequence
