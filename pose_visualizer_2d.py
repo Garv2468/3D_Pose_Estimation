ee3c36841e8743d76d94a2aed1b93f8e7f0c28c1
Garv2468 <garvmalik873@gmail.com>
2026-06-16 22:31:15 +0530

    Test this file once before moving forward

--- pose_visualizer_2d.py
+++ pose_visualizer_2d.py
@@ -3,12 +3,12 @@ import cv2
 import numpy as np
 from constants import BONES
 
-def draw_skeleton(frame: np.ndarray, df : pd.DataFrame, frame_num : int):
+def draw_skeleton(frame: np.ndarray, df : pd.DataFrame, frame_num : int, width, height):
     img = frame
     for i in BONES:
         if i != "SPINE" :
-            stp = (int(df.iloc[frame_num][f"{BONES[i][0]}.x"]), int(df.iloc[frame_num][f"{BONES[i][0]}.y"]))
-            etp = (int(df.iloc[frame_num][f"{BONES[i][1]}.x"]), int(df.iloc[frame_num][f"{BONES[i][1]}.y"]))
+            stp = (int(df.iloc[frame_num][f"{BONES[i][0]}.x"]*width), int(df.iloc[frame_num][f"{BONES[i][0]}.y"]*height))
+            etp = (int(df.iloc[frame_num][f"{BONES[i][1]}.x"]*width), int(df.iloc[frame_num][f"{BONES[i][1]}.y"]*height))
             cv2.line(img, stp, etp, (0, 255, 0), 2)
     return img
 
@@ -24,7 +24,7 @@ def save_annoted_video(cap, metadata, df):
         success, frame = cap.read_frame()
         if not success:
             break
-        img = draw_skeleton(frame, df, frame_num)
+        img = draw_skeleton(frame, df, frame_num, width, height)
         output.write(img)
         frame_num += 1
     output.release()
